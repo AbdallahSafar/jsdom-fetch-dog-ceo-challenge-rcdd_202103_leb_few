9cdee4b6d298e8c4f2bfc2e98cb7ec7249bd2ddd
Abdallah Safar <abdallah.safar01@gmail.com>
2021-04-25 12:23:12 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 console.log('%c HI', 'color: firebrick')
+
+function fetchImages(){
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+  const obj;
+  fetch(imgUrl).then(response => response.json()).then(json => obj = json);
+  return obj;
+}

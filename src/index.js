083c8b5aa959510991e672951d83dc7847750142
Abdallah Safar <abdallah.safar01@gmail.com>
2021-04-25 13:08:54 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ function filterBreeds() {
   console.log(letter);
   const li = document.querySelectorAll("li");
   for(let i=0;i<li.length;i++) {
-    li[i].style.display = "block";
+    li[i].style.display = "inline-block";
     if(li[i].innerHTML[0] !== letter) {
       li[i].style.display = "none";
     }

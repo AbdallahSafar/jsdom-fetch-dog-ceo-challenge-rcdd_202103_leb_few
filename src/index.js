ccee48e4076ce566a441f2ae93590fc5ab4e1f25
Abdallah Safar <abdallah.safar01@gmail.com>
2021-04-25 12:40:57 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 function fetchImgs() {
   const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
-  let imgDiv = document.getElementById("dog-image-container");
+  const imgDiv = document.getElementById("dog-image-container");
   fetch(imgUrl).then(response => response.json()).then(json => {
     console.log(json);
     for(let i=0;i<json.message.length;i++) {
@@ -12,7 +12,14 @@ function fetchImgs() {
 }
 
 function fetchBreeds() {
-
+  const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+  const imgDiv = document.getElementById("dog-breeds");
+  fetch(imgUrl).then(response => response.json()).then(json => {
+    console.log(json);
+    for(let i=0;i<json.message.length;i++) {
+      imgDiv.insertAdjacentHTML('beforeend',`<img src = ${json.message[i]} width='300px' height='200px' style='margin-right: 10px'>`);
+    }
+  });
 }
 
 fetchImgs();
